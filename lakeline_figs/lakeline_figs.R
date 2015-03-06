c9752bbe717ef4e996dc453d5fdfdd8798bc3bec
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-03-06 14:05:41 -0500

    draft figure

--- lakeline_figs/lakeline_figs.R
+++ lakeline_figs/lakeline_figs.R
@@ -7,22 +7,82 @@
 library(ggplot2)
 library(dplyr)
 
-dat<-read.csv("Data2014/Data2014.csv")
+dat <- read.csv("Data2014/Data2014.csv")
+dat_clean <- dat %>%
+  filter(SampleLocation != "Other" || 
+           SampleLocation != "Calibration" ||
+           SampleLocation != "Blank") %>%
+  filter(!Flag) %>%
+  filter(Fluorometer == "Beagle")%>%
+  filter(Units=="ug/l")%>%
+  filter(!is.na(Longitude))%>%
+  filter(!is.na(Latitude))
 
-dat %>%
-  select(State,Parameter,Value)%>%
-  filter(Parameter=="Chlorophyll")%>%
-  na.omit()%>%
-  group_by(State)%>%
-  summarize(chla_mn = mean(Value))%>%
-  ggplot(aes(x=State,y=chla_mn)) +
-    geom_bar(stat="identity")
+my_boot<-function(x,R,type=c("median","mean"),...){
+  b_md<-vector("numeric",R)
+  type<-match.arg(type)
+  for(i in 1:R){
+    b<-sample(x,length(x),replace=T)
+    if(type=="median"){
+      b_md[i]<-median(b,...)
+    } else if (type == "mean"){
+      b_md[i]<-mean(b,...)
+    } else {
+      stop("type must be median or mean")
+    }
+  }
+  return(c(quantile(b_md,c(0.025,0.975)),median(b_md)))
+}
 
-dat %>%
-  select(State,Parameter,Value)%>%
-  filter(Parameter=="Phycocyanin")%>%
+dat_bar <- dat_clean %>%
+  select(State,Parameter,Value,Units)%>%
   na.omit()%>%
-  group_by(State)%>%
-  summarize(phyco_mn = mean(Value))%>%
-  ggplot(aes(x=State,y=phyco_mn)) +
-  geom_bar(stat="identity")
+  group_by(State,Parameter)%>%
+  summarize(value_mdn = median(Value),
+            value_mn = mean(Value),
+            value_mdn_boot = my_boot(Value,1000,"median")[3],
+            value_mn_boot = my_boot(Value,1000,"mean")[3],
+            value_se = sd(Value)/sqrt(n()),
+            value_iqr = IQR(Value),
+            value_mdn_bootLL = my_boot(Value,1000,"median")[1],
+            value_mdn_bootUL = my_boot(Value,1000,"median")[2],
+            value_mn_bootLL = my_boot(Value,1000,"mean")[1],
+            value_mn_bootUL = my_boot(Value,1000,"mean")[2]
+            )
+
+limits_mn_se<-aes(ymax=value_mn+value_se, ymin=value_mn-value_se)
+limits_mdn_iqr<-aes(ymax=value_mdn+value_iqr, ymin=value_mdn-value_iqr)
+limits_mdn_boot<-aes(ymax=value_mdn_bootUL,ymin=value_mdn_bootLL)
+
+dodge<-position_dodge(width=0.9)
+state_bar_mn_se <- dat_bar %>% 
+  ggplot(aes(x=State,y=value_mn,fill=Parameter)) +
+    geom_bar(position="dodge",stat="identity") +
+    geom_errorbar(limits_mn_se, position=dodge, width=0.25)
+
+state_bar_mn_se
+
+state_bar_mdn_iqr <- dat_bar %>% 
+  ggplot(aes(x=State,y=value_mdn,fill=Parameter)) +
+  geom_bar(position="dodge",stat="identity") +
+  geom_errorbar(limits_mdn_iqr, position=dodge, width=0.25)
+
+state_bar_mdn_iqr
+
+state_bar_mdn_boot <- dat_bar %>% 
+  ggplot(aes(x=State,y=value_mdn_boot,fill=Parameter)) +
+  geom_bar(position="dodge",stat="identity") +
+  geom_errorbar(limits_mdn_boot, position=dodge, width=0.25)
+
+state_bar_mdn_boot
+
+#dat_clean %>%
+#  select(State,Parameter,Value,Units)%>%
+#  filter(Parameter=="Phycocyanin")%>%
+#  na.omit()%>%
+#  group_by(State)%>%
+#  summarize(phyco_mn = mean(Value))%>%
+#  ggplot(aes(x=State,y=phyco_mn)) +
+#  geom_bar(stat="identity")
+
+  
