60cd77a2fe097d4534a52fe24e418846c7ce6451
Bryan Milstead <willbmisled@gmail.com>
2015-03-23 16:10:22 -0400

    housekeeping changes

--- Analysis/mapNewEnglandCyanoIncidents.R
+++ Analysis/mapNewEnglandCyanoIncidents.R
@@ -45,66 +45,11 @@ library(maps)
   gg<-gg + annotate("text", x = -71.4, y = 41.3, label = "RI")
   gg
   
+
+
+#save the map
   ggsave("Analysis/mapClosures.tiff",gg,dpi=300)
   
   
 
 
-
-
-
-
-
-
-
-windows(7,7)
-  gg<-ggplot()
-  gg<-gg + geom_map(data=NE, map=NE,
-                       aes(x=long, y=lat, map_id=region, group=group),fill="#ffffff",color="#7f7f7f", size=.5) 
-  gg<-gg + geom_point(data=Cyano,
-                         aes(x=Centroid_Long, y=Centroid_Lat,size=5),
-                         color="#299868")
-  gg<-gg + coord_map("albers", lat0=39, lat1=45)
-  #gg<-gg + theme_bw()  #remove background color and change gridlines to grey
-  #gg<-gg + theme(legend.position="none") #no legend
-  gg<-gg + xlab("Longitude")
-  gg<-gg + ylab("Latitude")
-  gg<-gg + theme(axis.title= element_text(face="bold", size=20))
-  gg<-gg + theme(axis.text=element_text(angle=0, vjust=0.5, size=16))
-  gg<-gg +ggtitle("mapNewEnglandCyanoIncidents.R")
-  gg<-gg + annotate("text", x = -69, y = 46, label = "ME")
-  gg<-gg + annotate("text", x = -71.5, y = 44.2, label = "NH")
-  gg<-gg + annotate("text", x = -72.8, y = 44.2, label = "VT")
-  gg<-gg + annotate("text", x = -72.6, y = 42.4, label = "MA")
-  gg<-gg + annotate("text", x = -72.7, y = 41.6, label = "CT")
-  gg<-gg + annotate("text", x = -71.4, y = 41.2, label = "RI")
-  gg
-
-ggsave("Analysis/mapClosures.tiff",gg,dpi=300)
-
-
-
-
-gg<-ggplot()
-gg<-gg + geom_map(data=NE, map=NE,
-                  aes(x=long, y=lat, map_id=region, group=group),fill="#ffffff",color="#7f7f7f", size=.5) 
-gg<-gg + theme(
-  panel.background = element_blank(),
-  panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                  colour = "red"), 
-  panel.grid.minor = element_blank(),
-  legend.position="none"
-)
-gg
-
-
-
-myplot + theme(panel.background = element_blank())
-
-panel.background = element_rect(fill = "lightblue",
-                                colour = "lightblue",
-                                size = 0.5, linetype = "solid"),
-
-
-panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                colour = "green")
